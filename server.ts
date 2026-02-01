d911600c86a6ec255421beb1a3e47a85a657e86d
p122a-10v3r <usukhuu.lkha@gmail.com>
2026-02-02 01:35:10 +0800

    final

--- server.ts
+++ server.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { PrismaClient } from ".//src/generated/prisma/client.js";
+import { PrismaClient } from "./src/generated/prisma/client.js";
 import { PrismaPg } from "@prisma/adapter-pg";
 import pg from "pg";
 
